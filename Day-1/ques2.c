2199e0c001809c8e589c3cee3bdb630d102afd66
Diksha Swaroop <04dikshaswaroop@gmail.com>
2026-05-30 11:37:21 +0530

    Initisl commmit

---
+++ Day-1/ques2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    pritf("Enter a number:");
+    scanf("%d",&n);
+
+    for(int i=3;i<=10;i++)
+    {
+        printf("%d%d=%d\n,n,i,n*i");
+    }
+    retur 0;
+}
