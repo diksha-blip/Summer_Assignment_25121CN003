6b30ecf74ba7c6cb030217f8f114e4fa8d35b7cb
Diksha Swaroop <04dikshaswaroop@gmail.com>
2026-07-16 20:44:20 +0530

    Day 26 Assignment

---
+++ Day - 26/ques101.c
@@ -0,0 +1,33 @@
+//program to create number guessing game 
+
+#include<stdio.h>
+#include<stdib.h>
+#include<time.h>
+
+int main()
+{
+    int number , guess , attemps = 0;
+
+    srand(time(0));
+    number = rand() % 100 + 1; //random number between 1 to 100
+
+    printf("Guess the number(1 to 100)\n ");
+
+    do 
+    {
+        printf("Enter your guess:");
+        scanf("%d" , &guess);
+        attempts++;
+
+        if (guess>number)
+        printf("Too high!\n");
+    else if(guess<number)
+        printf("Too low!\n");
+    else
+       printf ("Congratulations! You guessed the number in %d attempts.\n" , attemps);
+
+}
+while(guess != number  );
+
+return 0;
+}
---
+++ Day - 26/ques104.c
@@ -0,0 +1,41 @@
+//program to create quize application
+
+#include <stdio.h>
+
+int main()
+{
+    int answer ,score = 0;
+
+    printf("***** QUIZ APPLICATION ****** \n\n");
+
+    //question 1
+    printf("1. What is the capital of India ?\n");
+    printf("1. Mumbai\n2. New Delhi\n3. Kolkata\n4. Chennai\n");
+    printf("Enter your answer:");
+    scanf("%d" , &answer);
+
+    if(answer == 2)
+    score++;
+
+   //Question 2
+   printf("\n2. Which language is used for C programming\n");
+   printf("1.Compiler\n2. Interpreter\n3.Programming language\n4. Operating System\n");
+   printf("Enter your answer:");
+   scanf("%d" , &answer);
+     
+   if(answer == 3)
+   score++;
+
+   //Question 3
+   printf("\n3. How many days are there in a week?\n");
+   printf("1. 5\n2, 6\n3. 7\n4  .8\n");
+   printf("Enter your answer:");
+   scanf("%d" , &answer );
+
+   if(answer == 1)
+   score++;
+
+     printf("|nYour Score  = %d/3\n" , score);
+
+     return 0;
+}
